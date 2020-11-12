c2a96d94a2ebda75ac2da3e2762a4a3ee45b9f4b
jared Long <jlong4223@gmail.com>
2020-11-12 17:23:56 -0600

    created functionality to signing up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,20 @@
+const User = require("../models/user")
+const bcrypt = require('bcrypt')
+
 module.exports = {
-    new: newUser
+    new: newUser,
+    signup
 }
 
 function newUser(req, res){
     res.render('auth/new')
+}
+
+function signup(req, res){
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    User.create(req.body, function(error, newUser){
+        console.log(newUser)
+        res.redirect('/')
+        // res.send(req.body)
+    })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const authCtrl = require('../controllers/auth')
 
 router.get('/new', authCtrl.new)
+router.post('/signup', authCtrl.signup)
 
 module.exports = router
