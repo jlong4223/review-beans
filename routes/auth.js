02b5207d0cb0a789b622f14020bb15483693b238
jared Long <jlong4223@gmail.com>
2020-11-12 21:42:04 -0600

    added login functionality

--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,7 @@ const authCtrl = require('../controllers/auth')
 router.get('/new', authCtrl.new)
 router.post('/signup', authCtrl.signup)
 
+router.get('/login', authCtrl.newLogin)
+router.post('/login', authCtrl.login)
+
 module.exports = router
