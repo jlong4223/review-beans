31632cb4c96ee225cb16968c90c54dfa1142dacb
jared Long <jlong4223@gmail.com>
2020-11-17 09:35:42 -0600

    added authentication function and checking if authenticated

--- utils/authorization.js
+++ utils/authorization.js
@@ -9,13 +9,12 @@ module.exports = {
 
 //a function to add the user to a property called user on the request object
 function addUserToRequest(req, res, next){
-    //TODO check if user is added to request
-    // if(req.user) return next(); // req.user already added!
-    
-    if(req.session && req.session.userId) {
-       User.findById(req.session.userId, function(err, foundUser){
+    //check if user is added to request
+    if(req.user) return next(); // req.user already added!
+    if(req.session && req.session.userID) {
+       User.findById(req.session.userID, function(err, foundUser){
            req.user = foundUser;
-           console.log(req.user)
+        //    console.log(req.user)
            next(); 
        })
     } else{
@@ -26,7 +25,6 @@ function addUserToRequest(req, res, next){
 // a function to check if a user is authenticated
 
 function isAuthenticated(req, res, next){
-    if(req.user) return next()
+    if(req.user !== undefined) return next();
+    res.redirect('/login')
 }
-
-//find out where req.user is being defined in exprestagram and change it here
