3ab307ebd5119bd85c21d51459778a93590fd37d
jared Long <jlong4223@gmail.com>
2020-11-17 11:21:07 -0600

    working on create allowed for login users

--- controllers/coffees.js
+++ controllers/coffees.js
@@ -1,50 +1,64 @@
 //require the module
 // const coffee = require('../models/coffee')
 const Coffee = require('../models/coffee')
+const Allow = require('./auth')
 
 //set up exports
 module.exports = {
     index,
-    new: newCoffee, 
-    create, 
-    delete: deleteCoffee, 
-    show, 
-    update, 
+    new: newCoffee,
+    create,
+    delete: deleteCoffee,
+    show,
+    update,
     edit
 }
 
-function index(req, res){
+function index(req, res) {
     console.log('req.user inside of /coffees:', req.user)
-    Coffee.find({}, function(err, coffees){
+    Coffee.find({}, function (err, coffees) {
         res.render('coffees/index', {
             coffees
         })
     })
 }
 
-function newCoffee(req, res){
+function newCoffee(req, res) {
     res.render('coffees/new')
 }
 
-function create(req, res){
+// function create(req, res){
+//     if (Allow.login === true){
+//         const coffee = new Coffee(req.body)
+//         coffee.save(function(err){
+//             console.log(coffee)
+//             res.redirect('/coffees')
+//         })
+//     } else {
+//         console.log("login first")
+//     }
+// }
+
+function create(req, res) {
     const coffee = new Coffee(req.body)
-    coffee.save(function(err){
+    coffee.save(function (err) {
         console.log(coffee)
         res.redirect('/coffees')
     })
 }
 
 
-function deleteCoffee(req, res){
-    Coffee.findById(req.params.id, function(err, coffee){
+function deleteCoffee(req, res) {
+    Coffee.findById(req.params.id, function (err, coffee) {
         coffee.remove()
-        coffee.save(function(err){
+        coffee.save(function (err) {
             res.redirect('/coffees')
         })
     })
 }
-function show(req, res){
-    Coffee.findById(req.params.id, function(err, coffee){
+
+function show(req, res) {
+    Coffee.findById(req.params.id, function (err, coffee) {
         res.render('coffees/show', {
             coffee
         })
@@ -52,21 +66,21 @@ function show(req, res){
 }
 
 function update(req, res) {
-    Coffee.findById(req.params.id, function(err, coffee){
+    Coffee.findById(req.params.id, function (err, coffee) {
         // coffee.text = req.body.text
         coffee.shop = req.body.shop
         coffee.location = req.body.location
         coffee.coffeetype = req.body.coffeetype
-        coffee.save(function(err){
+        coffee.save(function (err) {
             res.redirect('/coffees')
         })
     })
-  }
+}
 
 function edit(req, res) {
-    Coffee.findById(req.params.id, function(err, coffee){
-    res.render('coffees/edit', {
-        coffee
+    Coffee.findById(req.params.id, function (err, coffee) {
+        res.render('coffees/edit', {
+            coffee
+        })
     })
-  })
 }
